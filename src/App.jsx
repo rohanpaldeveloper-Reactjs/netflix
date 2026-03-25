5dc074c4efe347aaae52e8adf9d02ffbe2165cf5
Rohan Pal <devrohanpal@gmail.com>
2026-03-25 16:02:09 +0530

    Newproject setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className="bg-black text-white p-10">
+      Tailwind Test
+    </div>
+    </>
+  )
+}
+
+export default App
